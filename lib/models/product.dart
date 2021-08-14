177ebea2968934fc16a197ea17fedb5aa8916fcf
salmanpary <salmanpary@gmail.com>
2021-08-15 02:23:52 +0530

    main file edited,product overviewscreen added,Product model declared,added some fonts

---
+++ lib/models/product.dart
@@ -0,0 +1,14 @@
+
+class Product{
+final String id;
+final String description;
+final String title;
+final String imageurl;
+final double price;
+bool isfavorite;
+Product(
+{
+  this.id,this.title,this.price,this.imageurl,this.description,this.isfavorite
+}
+    );
+}
