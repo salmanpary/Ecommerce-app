f2686278279b16785c66ad811749331daba56f3e
salmanpary <salmanpary@gmail.com>
2021-08-15 23:51:01 +0530

    styling icons,theme added

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ class MyApp extends StatelessWidget {
       title: "Salman shop",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.red,
+        fontFamily: 'Lato',
+
       ),
       home: Productsoverviewscreen(),
     );
--- lib/product_item.dart
+++ lib/product_item.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageurl;
-  ProductItem(this.id,this.title,this.imageurl);
+  ProductItem(this.id, this.title, this.imageurl);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(backgroundColor:Colors.black87 ,leading:IconButton(icon:Icon(Icons.favorite), onPressed: (){}) ,title:Text(title,textAlign: TextAlign.center,) ,trailing:IconButton(icon:Icon(Icons.add_shopping_cart) , onPressed:(){
+    return ClipRRect(
+      borderRadius:BorderRadius.circular(10.0),
 
-      }) ,
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(icon: Icon(Icons.favorite), onPressed: () {},
+          color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing:
+              IconButton(icon: Icon(Icons.add_shopping_cart), onPressed: () {},
+              color: Colors.red,),
+        ),
+        child: Image.network(
+          imageurl,
+          fit: BoxFit.cover,
+        ),
       ),
-
-      child: Image.network(
-        imageurl,
-      fit: BoxFit.cover,
-    ),
-
     );
   }
 }
